07a5ccef251f35c2771deb5d7096eaa6de6af529
Micah Magruder <Micah.Magruder@cerner.com>
2018-02-23 16:30:51 -0500

    Added some error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,34 @@
 require('dotenv').config()
 
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    address: {
+      demand: true,
+      alias: 'a',
+      describe: 'Address to fetch weather data.',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url: process.env.MAPS_API_URL,
+  url: `${process.env.MAPS_API_URL}&key=${process.env.MAPS_API_KEY}&address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  if (error) {
+    console.log('There was an error connecting to Google Geocoding service.');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log("Unable to find that address.");
+  } else if (body.status === 'OK'){
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
+  console.log(`Longitute: ${body.results[0].geometry.location.lng}`)
+}
 })
