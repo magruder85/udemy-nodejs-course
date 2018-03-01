72afdd09b9d6db5ffe5ecdb3b0a45d2f11e63235
Micah Magruder <Micah.Magruder@cerner.com>
2018-03-01 16:23:00 -0500

    Getting ready to deploy to heroku

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+
+  console.log(log)
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server log')
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) =>{
+//   res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrenYear', () => {
@@ -33,6 +54,8 @@ app.get('/about', (req, res) => {
 
 app.get('/bad', (req, res) => {
   res.send({errorMessage: 'Something bad happened'});
-})
+});
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
